1789c3cdd1855c07c7064431bab10580f41e8643
Charles Liu <charlesliu.2012@gmail.com>
2016-09-20 17:06:47 -0400

    modify class and add getters

--- Event.py
+++ Event.py
@@ -8,3 +8,15 @@ class Event:
 
     def has_completed(self):
         return self.time != -1
+
+
+'''
+mock data
+from Runner import Runner
+from Event import Event
+e1 = Event("2014-09-28", "Marathon Oasis de Montreal", "Marathon", "2:03:05", "M40-45")
+e2 = Event("2013-09-29", "Marathon Oasis de Montreal", "Marathon", "2:00:05", "M40-45")
+e3 = Event("2012-09-27", "Marathon Oasis de Montreal", "Demi-Marathon", "1:03:05", "M40-45")
+e4 = Event("2013-08-01", "Marathon Oasis de Montreal", "Marathon", "1:03:05", "M40-45")
+r = Runner(1, [e1,e2,e3,e4], "M", 24)
+'''
--- Runner.py
+++ Runner.py
@@ -1,13 +1,34 @@
 class Runner:
-    def __init__(self, id, events):
+    def __init__(self, id, events, gender, age):
+
         self.id = id
         self.events = sorted(events, key = lambda x: x.date, reverse=True)
+        # 0 = male, 1 = female
+        self.gender = gender
+        self.age = age
+
+    def get_id(self):
+        return self.id
+
+    def get_gender(self):
+        return self.gender
+
+    def get_age(self):
+        return self.age
+
+    def get_total_races(self):
+        return len(self.events)
+
+    def get_event(self, event_name, event_year):
+        try:
+            #  and x.date[:4] == event_year
+            return filter(lambda x: event_name in x.name and x.date[:4] == event_year, self.events)
+        except:
+            # no participation == None
+            return None
 
     def get_latest_event(self):
         return self.events[-1]
-
-    def get_latest_category(self):
-        return self.get_latest_event().category
 
     def get_all_events(self):
         splitter =  "----------------------------------------"

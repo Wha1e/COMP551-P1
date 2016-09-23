f0a46baff6bd11aef0fd0cb6dd17a8e0b239a216
Maurice Zhang <Maurice@Maurices-MacBook-Air.local>
2016-09-23 00:10:22 -0400

    Implement use of cross validation

---
+++ models/cross_validation.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def cross_validate(X, Y, model_class, k=5):
+  combined_data = np.column_stack((X, Y))
+  np.random.shuffle(combined_data)
+  split_data = np.array_split(combined_data, k)
+  success_rates = []
+
+  for i in range(k):
+    training_data = np.concatenate([split_data[j] for j in range(k) if j != i])
+    validation_data = np.array(split_data[i])
+
+    X_training = training_data[:, :-1]
+    Y_training = training_data[:, [-1]]
+
+    model = model_class()
+    model.fit(X_training, Y_training)
+
+    X_validation = validation_data[:, :-1]
+    Y_validation = validation_data[:, [-1]]
+
+    model.predict(X_validation)
+    success_rate = model.get_success_rate(Y_validation)
+    success_rates.append(success_rate)
+    print("Fold #{}: {}".format(i + 1, success_rate))
+
+  print("Average success rate: {}".format(np.mean(success_rates)))
